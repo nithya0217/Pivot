5cdf443617342ba8cc738e1290c2c2ba6da5b616
nithya0217 <nithyag07@gmail.com>
2026-05-24 17:20:26 +0530

    commit

--- main.py
+++ main.py
@@ -4,32 +4,25 @@ from contextlib import asynccontextmanager
 from db import init_db, close_db
 import interactions
 
-# Lifespan context manager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Startup
     await init_db()
     yield
-    # Shutdown
     await close_db()
 
 app = FastAPI(lifespan=lifespan)
 
-# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://pivot-frontend-1lehl2hvt-nithya-s-projects3.vercel.app",
-        "https://pivot.up.railway.app",
         "http://localhost:3000",
-        "http://localhost:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Include routers
 app.include_router(interactions.router)
 
 @app.get("/")
